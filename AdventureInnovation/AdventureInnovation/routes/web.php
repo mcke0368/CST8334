8a171673066d49577ea1a016710931ae1ed5e492
Ian Arnald <arna0008@algonquinlive.com>
2018-02-26 10:42:50 -0500

    fixed editguide() in LoginController so user can edit their profile... also some text  formatting in web.php, which didn't show up...

--- AdventureInnovation/AdventureInnovation/routes/web.php
+++ AdventureInnovation/AdventureInnovation/routes/web.php
@@ -4,8 +4,7 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
+|| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -29,80 +28,82 @@ Route::get('homepage', function () {
 });
 
 /** Christian starts **/
-Route::get('/profile', 'LoginController@getGuide');
+    Route::get('/profile', 'LoginController@getGuide');
 
-Route::get('/profile/edit', 'LoginController@editGuide');
+    Route::get('/profile/edit', 'LoginController@editGuide');
 /** Christian ends **/
 
+
 /** Nadia  starts **/
-Route::get('/login', function () {
-    return view('login');
-});
+    Route::get('/login', function () {
+        return view('login');
+    });
 
-Route::get('/about', function () {
-    return view('about');
-});
+    Route::get('/about', function () {
+        return view('about');
+    });
 
-Route::get('/confirmation', function () {
-    return view('confirmaton');
-});
+    Route::get('/confirmation', function () {
+        return view('confirmaton');
+    });
 /** Nadia ends **/
 
+
 /** Lilian starts **/
-Route::get('/jobDetails', function () {
-    return view('jobDetails');
-});
+    Route::get('/jobDetails', function () {
+        return view('jobDetails');
+    });
 
-Route::get('/guideDashboard', function () {
-    return view('guideDashboard');
-});
+    Route::get('/guideDashboard', function () {
+        return view('guideDashboard');
+    });
 
-Route::get('/manageAccount', function () {
-    return view('manageAccount');
-});
+    Route::get('/manageAccount', function () {
+        return view('manageAccount');
+    });
 /** Lilian ends **/
 
 /** Ki Beom starts **/
 
-Route::get('/logout', 'LoginController@logout');
+    Route::get('/logout', 'LoginController@logout');
 
-Route::post('/saveType', 'LogbookTypeController@saveType');
+    Route::post('/saveType', 'LogbookTypeController@saveType');
 
-Route::get('contact', function()
-{
-    return view('contact');
-});
+    Route::get('contact', function()
+    {
+        return view('contact');
+    });
 
-Route::get('companyContact', function()
-{
-    return view('companyContact');
-});
+    Route::get('companyContact', function()
+    {
+        return view('companyContact');
+    });
 
-Route::get('logbook', 'LogbookListController@passLogbookData');
+    Route::get('logbook', 'LogbookListController@passLogbookData');
 
-Route::post('/saveLog', 'LogbookController@saveLog');
+    Route::post('/saveLog', 'LogbookController@saveLog');
 
-Route::post('/setLog', 'LogbookController@setLog');
+    Route::post('/setLog', 'LogbookController@setLog');
 
-Route::get('defaultLog', 'LogbookController@showLog');
+    Route::get('defaultLog', 'LogbookController@showLog');
 
-Route::post('/setLogType', 'LogbookController@setLogType');
+    Route::post('/setLogType', 'LogbookController@setLogType');
 
-Route::get('createDefaultLogbook', 'LogbookController@showLogTemplate');
+    Route::get('createDefaultLogbook', 'LogbookController@showLogTemplate');
 
-Route::get('createLogType', 'LogbookTypeController@showTemplate');
+    Route::get('createLogType', 'LogbookTypeController@showTemplate');
 
-Route::post('/deleteLogType', 'LogbookListController@deleteLogType');
+    Route::post('/deleteLogType', 'LogbookListController@deleteLogType');
 
-Route::post('/deleteLog', 'LogbookListController@deleteLog');
+    Route::post('/deleteLog', 'LogbookListController@deleteLog');
 
-Route::get('/deleteLogType', function()
-{
-    return view('logbook');
-});
+    Route::get('/deleteLogType', function()
+    {
+        return view('logbook');
+    });
 
-Route::get('/deleteLogType', function()
-{
-    return view('logbook');
-});
+    Route::get('/deleteLogType', function()
+    {
+        return view('logbook');
+    });
 /** Ki Beom ends **/
