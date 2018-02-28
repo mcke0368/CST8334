f3a3033a256985a28c00d633a267fd7c229943d6
Adam McLean <mclean.adam@gmail.com>
2018-02-28 07:10:51 -0500

    updated to ORM for guides, certs, users.  Working except for update profile

--- AdventureInnovation/AdventureInnovation/routes/web.php
+++ AdventureInnovation/AdventureInnovation/routes/web.php
@@ -148,4 +148,3 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-
