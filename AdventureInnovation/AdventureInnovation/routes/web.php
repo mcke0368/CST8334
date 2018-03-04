de1b3836c32fb040c7618cf8ebcb5e64bdcf22da
Adam McLean <mclean.adam@gmail.com>
2018-03-03 19:55:39 -0500

    cleaned out some of the old unused code and appended ajax to ajax function calls

--- AdventureInnovation/AdventureInnovation/routes/web.php
+++ AdventureInnovation/AdventureInnovation/routes/web.php
@@ -148,3 +148,4 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+
