1f1300306b84ee38aff8a7624b9cb965a21c94e1
Adam McLean <mclean.adam@gmail.com>
2018-03-04 04:51:06 -0500

    updated tests to make them work with new changes

--- AdventureInnovation/AdventureInnovation/tests/Feature/UserAuthenTest.php
+++ AdventureInnovation/AdventureInnovation/tests/Feature/UserAuthenTest.php
@@ -16,7 +16,9 @@ class UserAuthenTest extends TestCase
     {
         //simple test to make sure admin is in the db at all times
         //will later upgrade this to actual user authentication
-        $this->assertDatabaseHas('users',['email' => 'admin@localhost']);
-        $this->assertDatabaseHas('users',['password' => 'admin']);
+        $this->assertDatabaseHas('users',['firstname' => env('ADMIN_USER_NAME')]);
+        $this->assertDatabaseHas('users',['lastname' => env('ADMIN_USER_NAME')]);
+        $this->assertDatabaseHas('users',['username' => env('ADMIN_USER_NAME')]);
+        $this->assertDatabaseHas('users',['email' => env('ADMIN_USER_EMAIL')]);
     }
 }
