79cc411e6cee1c9826496c6b10add14b0129e9cb
Adam McLean <mclean.adam@gmail.com>
2018-03-13 23:53:46 -0400

    rebase against master

--- AdventureInnovation/AdventureInnovation/public/js/extra.js
+++ AdventureInnovation/AdventureInnovation/public/js/extra.js
@@ -126,7 +126,6 @@ function dropdown_logbooks_callback() {
 /* Logout requires a post request so add this function to the onclick of logout button */
 function dropdown_logout_callback() {
 
-
     $.ajax({
         type: "POST",
         url: '/logout',
