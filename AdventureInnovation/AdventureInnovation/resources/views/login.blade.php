ef7745bd3bd3ba57232747b4c5b69b72d842e1e5
ThanhTrac <31932014+ThanhTrac@users.noreply.github.com>
2018-03-21 10:09:43 -0400

    Changing more in the css file (#77)

--- AdventureInnovation/AdventureInnovation/resources/views/login.blade.php
+++ AdventureInnovation/AdventureInnovation/resources/views/login.blade.php
@@ -7,30 +7,27 @@
 @section('body')
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-6" style="padding: 20px;margin-top:15px;">
-				<div class="col-sm-12" style="padding: 20px;margin-top:15px;">
+			<div class="col-sm-6 login-pad">
+				<div class="col-sm-12 login-pad">
                     @include('auth.register-partial')
 				</div>
 			</div>
 			
 			<div class="col-sm-12 col-md-6">
-				<div class="col-sm-12 text-right" style="padding: 20px;margin-top:15px;">
+				<div class="col-sm-12 text-right login-pad">
 					<div class="col-sm-12">
 						Interested in creating a Company Account?
 					</div>
 					<div class="col-sm-12 text-right">
 						<a href="/companyLogin" class="btn btn-primary">Inquiry Here</a>
-						<!--
-						<button class="btn btn-danger">Inquiry Here</button>
-						-->
 					</div>
 				</div>
-				<div class="col-sm-12 " style="padding: 20px;margin-top:15px; ">
+				<div class="col-sm-12 login-pad">
                     @include('auth.login-partial')
 				</div>
-				<div class="col-sm-12 text-right" style="padding: 20px; margin-top:10px">
+				<div class="col-sm-12 text-right">
 					<p>English</p>
-					<p><a href="#" style="color: black;text-decoration: underline;">Change Language?</a></p>
+					<p><a href="#" class="change-language">Change Language?</a></p>
 				</div>
 			</div>
 		</div>
